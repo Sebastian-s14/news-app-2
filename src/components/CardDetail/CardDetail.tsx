b15fabf28585f3699c01b218e42b102a04928fa3
Sebastian-S14 <sebastian.sanchez@tecsup.edu.pe>
2023-02-11 10:17:56 -0500

    feat: :sparkles: add react-helmet

--- src/components/CardDetail/CardDetail.tsx
+++ src/components/CardDetail/CardDetail.tsx
@@ -1,3 +1,5 @@
+import { Helmet } from 'react-helmet'
+
 import type { Product } from '../../interfaces'
 import { CardListImage } from '../CardListImage'
 
@@ -9,13 +11,21 @@ interface CardDetailProps {
 
 export const CardDetail = ({ product }: CardDetailProps) => {
   return (
-    <article className="card-detail">
-      {/* <Card product={data?.products[0]} maxWidth={500} /> */}
-      {/* <Slideshow images={data?.products[0].images ?? []} /> */}
-      <h2>{product?.title}</h2>
-      <img src={product?.images[0]} alt="" />
-      <p>{product?.description}</p>
-      <CardListImage images={product?.images ?? []} />
-    </article>
+    <>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>{product?.title}</title>
+        <meta name="description" content={product?.description} />
+        {/* <link rel="canonical" href="http://mysite.com/example" /> */}
+      </Helmet>
+      <article className="card-detail">
+        {/* <Card product={data?.products[0]} maxWidth={500} /> */}
+        {/* <Slideshow images={data?.products[0].images ?? []} /> */}
+        <h2>{product?.title}</h2>
+        <img src={product?.images[0]} alt="" />
+        <p>{product?.description}</p>
+        <CardListImage images={product?.images ?? []} />
+      </article>
+    </>
   )
 }
